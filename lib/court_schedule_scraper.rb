1d044030aa26b746a814558c5a0624a3ac78326b
Tom Dooner <tom.dooner@brigade.com>
2017-05-24 13:41:23 -0700

    Initial commit with scraper from myadvocate

---
+++ lib/court_schedule_scraper.rb
@@ -0,0 +1,77 @@
+require 'mechanize'
+require 'json'
+
+class CourtScheduleScraper
+  def cases_for(date)
+    return to_enum(:cases_for, date) unless block_given?
+
+    mech = Mechanize.new
+    page = mech.get('https://publicaccess.courts.oregon.gov/PublicAccess/default.aspx')
+    page = mech.post('https://publicaccess.courts.oregon.gov/PublicAccess/Search.aspx?ID=900',
+                     NodeID: '104100,104210,104215,104220,104225,104310,104320,104330,104410,104420,104430,104440',
+                     NodeDesc: 'Multnomah')
+    # GNARLY
+    page = mech.post('https://publicaccess.courts.oregon.gov/PublicAccess/Search.aspx?ID=900',
+                     :__EVENTTARGET => '',
+                     :__EVENTARGUMENT => '',
+                     NodeID: '104100,104210,104215,104220,104225,104310,104320,104330,104410,104420,104430,104440',
+                     NodeDesc: 'Multnomah',
+                     SearchBy: '5',
+                     CaseSearchMode: 'CaseNumber',
+                     PartySearchMode: 'Name',
+                     AttorneySearchMode: 'Name',
+                     cboState: 'AA',
+                     CaseStatusType: '0',
+                     cboJudOffc: '19549',
+                     chkCriminal: 'on',
+                     chkFamily: 'on',
+                     chkCivil: 'on',
+                     chkProbate: 'on',
+                     chkDtRangeCriminal: 'on',
+                     chkDtRangeFamily: 'on',
+                     chkDtRangeCivil: 'on',
+                     chkDtRangeProbate: 'on',
+                     cboMagist: '49635',
+                     chkCriminalMagist: 'on',
+                     chkFamilyMagist: 'on',
+                     chkCivilMagist: 'on',
+                     chkProbateMagist: 'on',
+                     DateSettingOnAfter: date.strftime('%-m/%d/%Y'),
+                     DateSettingOnBefore: date.strftime('%-m/%d/%Y'),
+                     SortBy: 'fileddate',
+                     SearchSubmit: 'Search',
+                     SearchType: 'DATERANGE',
+                     SearchMode: 'DATERANGE',
+                     StatusType: 'true',
+                     AllStatusTypes: 'true',
+                     CaseCategories: 'CR,CV,FAM,PR',
+                     SearchParams: 'SearchBy~~Search By:~~Date Range~~Date Range||DateSettingOnAfter~~Date On or After:~~5/17/2017~~5/17/2017||DateSettingOnBefore~~Date On or Before:~~5/17/2017~~5/17/2017||selectSortBy~~Sort By:~~Filed Date~~Filed Date||CaseCategories~~Case Categories:~~CR,CV,FAM,PR~~Criminal, Civil, Family, Probate and Mental Health',
+                    )
+
+    schedule_table(page).css('> tr').each do |row|
+      next if row.text =~ /Case Number/ # skip header row
+
+      yield(
+        case_number: row.css('td:nth-child(1) tr:nth-child(1) td').text,
+        type: row.css('td:nth-child(1) tr:nth-child(2) td').text,
+        style: row.css('td:nth-child(2)').text,
+        judicial_officer: row.css('td:nth-child(3) tr:nth-child(1) td').text,
+        physical_location: row.css('td:nth-child(3) tr:nth-child(2) td').text,
+        date: row.css('td:nth-child(4) tr:nth-child(1) td').text,
+        time: row.css('td:nth-child(4) tr:nth-child(2) td').text,
+        hearing_type: row.css('td:nth-child(4) tr:nth-child(3) td').text
+      )
+    end
+  end
+
+  def schedule_table(page)
+    node = page.css('tr[bgcolor="#EEEEEE"]').first
+    (node = node.parent) until node.name == 'table'
+    node
+  end
+end
+
+date = ARGV[0] || Date.today
+schedule = CourtScheduleScraper.new.cases_for(date).to_a
+schedule.sort_by! { |c| c[:case_number] }
+puts JSON.pretty_generate(schedule)
